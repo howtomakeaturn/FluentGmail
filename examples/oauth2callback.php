78c972a86e759d05ec7e5a475db6135a68616df1
chuan-hao, you <howtomakeaturn@hotmail.com>
2015-03-14 01:48:09 +0800

    Move example code to example folder.

--- examples/oauth2callback.php
+++ examples/oauth2callback.php
@@ -1,5 +1,5 @@
 <?php
-    require_once('vendor/autoload.php');
+    require_once('../vendor/autoload.php');
 
     use \Howtomakeaturn\AskGmail\Authenticator;
     use \Howtomakeaturn\AskGmail\GoogleClientManager;
@@ -10,17 +10,7 @@
 
     $clientManager = new GoogleClientManager(new Google_Client());
 
-    $config = [
-        'clientId' => '198663035027-fucqv7fhr8co4svrfdosmlt0mm4jquev.apps.googleusercontent.com',  
-        'clientSecret' => 'lejFNh1LkYzlPQld00Xj5iKB',
-        'redirectUri' => 'http://gmail/oauth2callback',
-        'scopes' => [
-            'https://www.googleapis.com/auth/gmail.modify',
-            'https://www.googleapis.com/auth/gmail.readonly',
-            'https://www.googleapis.com/auth/gmail.compose',
-            'https://mail.google.com/'
-        ]
-    ];
+    $config = require('config.php');
 
     $clientManager->setConfig($config);
 
